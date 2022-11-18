1b24edf169b0ae71701de220a174c630e6755c88
Emilio López <emilio.lopez@trailofbits.com>
2022-11-18 15:00:28 -0300

    Add a way to specify remote builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,15 +35,20 @@ if [ "$EVENT_TYPE" = "closed" ]; then
   exit 0
 fi
 
+remote_build=
+if [ "$INPUT_REMOTE" != "false" ]; then
+  remote_build="--remote-only"
+fi
+
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
   flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
-  flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate $remote_build
 elif [ "$INPUT_UPDATE" != "false" ]; then
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate $remote_build
 fi
 
 # Attach postgres cluster to the app if specified.
